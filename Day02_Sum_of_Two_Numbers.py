4a0465e6efff91bdde8fac23c9f8c1ab1f200c99
Sujatha2108 <143613001+Sujatha2108@users.noreply.github.com>
2025-07-27 20:31:01 +0530

    Create Day02_Sum_of_Two_Numbers.py

---
+++ Day02_Sum_of_Two_Numbers.py
@@ -0,0 +1,6 @@
+def sum(a,b):
+    return a+b
+num_a=int(input("Enter 1st number: "))
+num_b=int(input("Enter 2st number: "))
+add=sum(num_a,num_b)
+print(f"The sum of {num_a} and {num_b} is:{add}")
