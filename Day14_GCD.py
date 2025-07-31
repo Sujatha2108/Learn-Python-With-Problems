f2e327157cc0c468f84bee26952b42eaaa196964
Sujatha2108 <143613001+Sujatha2108@users.noreply.github.com>
2025-07-31 17:38:57 +0530

    Update Day14_GCD.py

--- Day14_GCD.py
+++ Day14_GCD.py
@@ -7,3 +7,12 @@ num2 = int(input("Enter second number: "))
 
 gcd = find_gcd(num1, num2)
 print(f"The GCD of {num1} and {num2} is: {gcd}")
+
+**method-2**
+import math
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+gcd = math.gcd(num1, num2)
+print(f"The GCD of {num1} and {num2} is: {gcd}")
